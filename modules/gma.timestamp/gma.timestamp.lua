424aa9a1cee331040b06880d6289c5c5dcbb887d
Mac Tirney <mactirney@gmail.com>
2023-12-12 19:53:32 -0500

    feat: add gma.timestamp files to modules
    Add gma.timestamp functions to a markdown file in a seperate folder.
    Create gma.timestamp ldoc to highlight how to use each function.

---
+++ modules/gma.timestamp/gma.timestamp.lua
@@ -0,0 +1,18 @@
+-- gma.timestamp Module - GMA2 Lua Functions
+-- Module: gma.timestamp
+
+
+-- .name(var)
+-- param - var: Number
+-- returns: String
+function gma.timestamp.name(var) end
+
+-- .time(var)
+-- param - var: Number
+-- returns: Float
+function gma.timestamp.time(var) end
+
+-- .amount()
+-- returns: Number
+-- usage: gma.timestamp.amount()
+function gma.timestamp.amount() end
